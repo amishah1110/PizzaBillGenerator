28813b84e73884295c4eba8beb4d29d4d08b8996
amishah1110 <127313122+amishah1110@users.noreply.github.com>
2023-06-13 11:12:20 +0530

    Add files via upload

---
+++ PizzaBillGenerator.java
@@ -0,0 +1,16 @@
+
+package pizzabillgenerator;
+
+public class PizzaBillGenerator {
+public static void main(String[] args) {
+       
+		
+		DeluxePizza dp=new DeluxePizza(false);
+		dp.addExtraTomato();
+		dp.addExtraCheese();
+                dp.takeaway();
+		dp.getBill();
+		dp.getBillWithTax();
+    }
+    
+}
